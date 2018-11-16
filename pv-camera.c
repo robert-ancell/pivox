ef2fa5481dce90cc42abe7fb03bbbe3ae3d91d2d
Robert Ancell <robert.ancell@gmail.com>
2018-11-17 00:11:20 +1300

    Refactor vector/matrix functions

--- pv-camera.c
+++ pv-camera.c
@@ -28,14 +28,14 @@ struct _PvCamera
 G_DEFINE_TYPE (PvCamera, pv_camera, G_TYPE_OBJECT)
 
 static void
-vector_make (GLfloat  x,
+vector_make (GLfloat *v,
+             GLfloat  x,
              GLfloat  y,
-             GLfloat  z,
-             GLfloat *result)
+             GLfloat  z)
 {
-    result[0] = x;
-    result[1] = y;
-    result[2] = z;
+    v[0] = x;
+    v[1] = y;
+    v[2] = z;
 }
 
 static void
@@ -50,95 +50,96 @@ vector_normalize (GLfloat *v)
 }
 
 static void
-vector_cross (GLfloat *a,
-              GLfloat *b,
-              GLfloat *result)
+vector_cross (GLfloat *v,
+              GLfloat *a,
+              GLfloat *b)
 {
-    vector_make (a[1]*b[2] - a[2]*b[1],
+    vector_make (v,
+                 a[1]*b[2] - a[2]*b[1],
                  a[2]*b[0] - a[0]*b[2],
-                 a[0]*b[1] - a[1]*b[0],
-                 result);
+                 a[0]*b[1] - a[1]*b[0]);
 }
 
 static void
-matrix_make (GLfloat v00, GLfloat v10, GLfloat v20, GLfloat v30,
+matrix_make (GLfloat *m,
+             GLfloat v00, GLfloat v10, GLfloat v20, GLfloat v30,
              GLfloat v01, GLfloat v11, GLfloat v21, GLfloat v31,
              GLfloat v02, GLfloat v12, GLfloat v22, GLfloat v32,
-             GLfloat v03, GLfloat v13, GLfloat v23, GLfloat v33,
-             GLfloat *result)
+             GLfloat v03, GLfloat v13, GLfloat v23, GLfloat v33)
 {
-    result[ 0] = v00;
-    result[ 1] = v10;
-    result[ 2] = v20;
-    result[ 3] = v30;
-    result[ 4] = v01;
-    result[ 5] = v11;
-    result[ 6] = v21;
-    result[ 7] = v31;
-    result[ 8] = v02;
-    result[ 9] = v12;
-    result[10] = v22;
-    result[11] = v32;
-    result[12] = v03;
-    result[13] = v13;
-    result[14] = v23;
-    result[15] = v33;
+    m[ 0] = v00;
+    m[ 1] = v10;
+    m[ 2] = v20;
+    m[ 3] = v30;
+    m[ 4] = v01;
+    m[ 5] = v11;
+    m[ 6] = v21;
+    m[ 7] = v31;
+    m[ 8] = v02;
+    m[ 9] = v12;
+    m[10] = v22;
+    m[11] = v32;
+    m[12] = v03;
+    m[13] = v13;
+    m[14] = v23;
+    m[15] = v33;
 }
 
 static void
-matrix_make_projection (GLfloat  fov_y,
+matrix_make_projection (GLfloat *m,
+                        GLfloat  fov_y,
                         GLfloat  aspect,
                         GLfloat  z_near,
-                        GLfloat  z_far,
-                        GLfloat *result)
+                        GLfloat  z_far)
 {
     GLfloat f = atanf (fov_y / 2.0f);
     GLfloat a = f / aspect;
     GLfloat b = (z_far + z_near) / (z_near - z_far);
     GLfloat c = (2.0f * z_far * z_near) / (z_near - z_far);
-    matrix_make ( a,  0,  0,  0,
+    matrix_make ( m,
+                  a,  0,  0,  0,
                   0,  f,  0,  0,
                   0,  0,  b,  c,
-                  0,  0, -1,  0,
-                 result);
+                  0,  0, -1,  0);
 }
 
 static void
-matrix_make_direction (GLfloat *dir,
-                       GLfloat *up,
-                       GLfloat *result)
+matrix_make_direction (GLfloat *m,
+                       GLfloat *dir,
+                       GLfloat *up)
 {
     GLfloat s[3];
-    vector_cross (dir, up, s);
+    vector_cross (s, dir, up);
     GLfloat u[3];
-    vector_cross (dir, s, u);
-    matrix_make (   s[0],    s[1],    s[2], 0,
+    vector_cross (u, dir, s);
+    matrix_make (m,
+                    s[0],    s[1],    s[2], 0,
                     u[0],    u[1],    u[2], 0,
                  -dir[0], -dir[1], -dir[2], 0,
-                       0,       0,       0, 1,
-                 result);
+                       0,       0,       0, 1);
 }
 
 static void
-matrix_mult (GLfloat *a, GLfloat *b, GLfloat *result)
+matrix_mult (GLfloat *m, GLfloat *a, GLfloat *b)
 {
     for (int row = 0; row < 4; row++) {
         for (int col = 0; col < 4; col++) {
-            result[col * 4 + row] = 0;
+            m[col * 4 + row] = 0;
             for (int i = 0; i < 4; i++)
-                result[col * 4 + row] += a[col * 4 + i] * b[i * 4 + row];
+                m[col * 4 + row] += a[col * 4 + i] * b[i * 4 + row];
         }
     }
 }
 
 static void
-matrix_translate (GLfloat x, GLfloat y, GLfloat z, GLfloat *result)
+matrix_make_translate (GLfloat *m,
+                       GLfloat x, GLfloat y, GLfloat z)
 {
-    matrix_make (1, 0, 0, x,
+    matrix_make (m,
+                 1, 0, 0, x,
                  0, 1, 0, y,
                  0, 0, 1, z,
-                 0, 0, 0, 1,
-                 result);
+                 0, 0, 0, 1);
 }
 
 void
@@ -190,23 +191,23 @@ pv_camera_transform (PvCamera *self,
     g_return_if_fail (PV_IS_CAMERA (self));
 
     GLfloat proj[16];
-    matrix_make_projection (M_PI / 3.0f, (GLfloat) width / height, 0.1f, 100.0f, proj);
+    matrix_make_projection (proj, M_PI / 3.0f, (GLfloat) width / height, 0.1f, 100.0f);
 
     GLfloat trans[16];
-    matrix_translate (self->x, self->y, self->z, trans);
+    matrix_make_translate (trans, self->x, self->y, self->z);
     GLfloat rot[16];
     GLfloat up[3], dir[3];
-    vector_make (0, 0, 1, up);
-    vector_make (self->target_x - self->x,
+    vector_make (up, 0, 0, 1);
+    vector_make (dir,
+                 self->target_x - self->x,
                  self->target_y - self->y,
-                 self->target_z - self->z,
-                 dir);
+                 self->target_z - self->z);
     vector_normalize (dir);
-    matrix_make_direction (dir, up, rot);
+    matrix_make_direction (rot, dir, up);
     GLfloat t[16];
-    matrix_mult (proj, rot, t);
+    matrix_mult (t, proj, rot);
     GLfloat mvp[16];
-    matrix_mult (t, trans, mvp);
+    matrix_mult (mvp, t, trans);
 
     glUniformMatrix4fv (uniform_location, 1, GL_TRUE, mvp);
 }
