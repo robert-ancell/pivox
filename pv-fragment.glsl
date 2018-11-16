7542e6db4e505c914ac109cb743f11b2b8772a95
Robert Ancell <robert.ancell@gmail.com>
2018-11-16 22:27:35 +1300

    Set color in vertex shader

--- pv-fragment.glsl
+++ pv-fragment.glsl
@@ -1,4 +1,8 @@
+#version 330
+
+in vec3 Color;
+
 void main ()
 {
-  gl_FragColor = vec4 (1.0, 1.0, 1.0, 1.0);
+  gl_FragColor = vec4 (Color, 1.0);
 };
--- pv-vertex.glsl
+++ pv-vertex.glsl
@@ -1,6 +1,13 @@
+#version 330
+
 attribute vec4 position;
+
+out vec3 Color;
+
 uniform mat4 MVP;
+
 void main ()
 {
+   Color = vec3 (1.0, 0.5, 0.5);
    gl_Position = MVP * vec4 (position.xyz, 1.0);
 };
